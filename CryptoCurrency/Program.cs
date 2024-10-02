a00c7e38a31c7f300351a2c3cbc005cbf4163c29
Rasmus <j.rasbr@gmail.com>
2024-10-02 20:44:46 +0200

    vbn

--- CryptoCurrency/Program.cs
+++ CryptoCurrency/Program.cs
@@ -2,6 +2,7 @@
     //Main method
     public static void Main(string[] args) {}
 
+    Dictionary<string, double> prices = new Dictionary<string, double>();
     /// <summary>
     /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
     /// Hvis der tidligere er angivet en værdi for samme kryptovaluta, 
@@ -11,6 +12,15 @@
     /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
     public void SetPricePerUnit(String currencyName, double price) {
 
+        //Hvis prisen er negativ, returneres uden at gøre noget
+        if (price <= 0)
+            return;
+
+        //Fjern nøglen. Hvis den ikke findes, smides der ingen exception.
+        prices.Remove(currencyName);
+
+        //Tilføj nøglen og værdien
+        prices.Add(currencyName, price);
     }
 
     /// <summary>
@@ -23,6 +33,18 @@
     /// <param name="amount">Beløbet angivet i valutaen angivet i fromCurrencyName</param>
     /// <returns>Værdien af beløbet i toCurrencyName</returns>
     public double Convert(String fromCurrencyName, String toCurrencyName, double amount) {
-        return 0;
+        //Hvis en af valutaerne ikke findes, kastes en ArgumentException
+        if (!prices.ContainsKey(fromCurrencyName) || !prices.ContainsKey(toCurrencyName))
+            throw new ArgumentException("Key did not exist");
+
+        //Hvis amount er negativ, kastes en ArgumentException
+        if (amount <= 0)
+            throw new ArgumentException("Amount was negative or 0");
+
+        //Hent priserne for de to valutaer i dictionary
+        double fromPrice = prices[fromCurrencyName];
+        double toPrice = prices[toCurrencyName];
+
+        return amount * toPrice / fromPrice;
     }
 }
